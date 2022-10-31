4139e6334baa4e4e29b8cda537b7a48421d10fa9
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2022-10-31 15:43:51 +0000

    Chnage StructuredText to String

--- gatsby-node.js
+++ gatsby-node.js
@@ -420,7 +420,7 @@ exports.createSchemaCustomization = async ({ actions }) => {
       description: String
       image: HomepageImage
       html: String!
-      body: StructuredText
+      body: String
     }
   `)
 
@@ -734,7 +734,7 @@ exports.createSchemaCustomization = async ({ actions }) => {
       image: HomepageImage
         @link(by: "originalId", from: "entityPayload.attributes.metadata.image")
       html: String! @richText
-      body: StructuredText
+      body: String
     }
   `)
 }
